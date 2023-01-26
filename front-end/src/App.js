ae719c2370dcd55a620e41a36018d0af40c29157
Marco Túlio Huara <marcotuliohuara@gmail.com>
2023-01-26 18:10:43 -0300

    feat: adiciona a rota para página de produtos e adiciona o provider context no arquivo index.

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,15 +1,12 @@
 import React from 'react';
-import './App.css';
-import rockGlass from './images/rockGlass.svg';
+import { Switch, Route } from 'react-router-dom';
+import Products from './pages/products';
 
 function App() {
   return (
-    <div className="App">
-      <span className="logo">TRYBE</span>
-      <object className="rocksGlass" type="image/svg+xml" data={ rockGlass }>
-        Glass
-      </object>
-    </div>
+    <Switch>
+      <Route path="/customer/products" component={ Products } />
+    </Switch>
   );
 }
 
