7bbb0290bdb4a603dcde4d6dcfb0c0d90929a631
Crisna Bezerra <crisnabto@gmail.com>
2023-01-25 21:58:56 -0300

    Cria axios para tela de registro

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const cors = require('cors');
 const loginRoutes = require('../routers/login.router');
+const registerRoutes = require('../routers/register.router') // +
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(cors());
+
 app.use('/login', loginRoutes);
+app.use('/register', registerRoutes); // +
 
 app.get('/coffee', (_req, res) => res.status(418).end());
 
--- front-end/src/services/api.js
+++ front-end/src/services/api.js
@@ -6,11 +6,14 @@ const port = process.env.REACT_APP_BACKEND_PORT || defaultPort;
 
 const api = axios.create({
   baseURL: `http://localhost:${port}`,
-  //   baseURL: 'http://localhost:3001',
 });
 
 export const validateLogin = async (email, password) => {
   const result = await api.post('/login', { email, password });
   return result.data;
 };
-export const validateLgin = (data) => api.post('/login', data);
+
+export const registerNewUser = async (name, email, password) => {
+  const result = await api.post('/register', { name, email, password });
+  return result.data;
+};
