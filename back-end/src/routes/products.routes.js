845b705f48a10af2666c391e329c9f85f67a08ca
Lizandra Debussy <lizandradebussy@hotmail.com>
2023-01-27 19:20:49 -0300

    Correção do jwt secret req.13

--- back-end/src/routes/products.routes.js
+++ back-end/src/routes/products.routes.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const productController = require('../controllers/products.controller');
-// const authMiddleware = require('../middlewares/auth.middleware');
+const authMiddleware = require('../middlewares/auth.middleware');
 
 const router = express.Router();
 
-router.get('/', productController.getAllProducts);
+router.get('/', authMiddleware, productController.getAllProducts);
 router.get('/:id', productController.getProductById);
 
 module.exports = router;
--- back-end/src/utils/jwt.utils.js
+++ back-end/src/utils/jwt.utils.js
@@ -1,21 +1,14 @@
 require('dotenv/config');
 const jwt = require('jsonwebtoken');
+const { readFileSync } = require('fs');
+const secret = readFileSync('jwt.evaluation.key', 'utf-8');
 
 const createToken = (data) => {
-  const token = jwt.sign({ data }, process.env.JWT_SECRET || 'secret_key', {
+  const token = jwt.sign({ data }, secret, {
     expiresIn: '1d',
     algorithm: 'HS256',
   });
   return token;
 };
 
-const validateToken = (token) => {
-  try {
-    const data = jwt.verify(token, process.env.JWT_SECRET || 'secret_key');
-    return data;
-  } catch (error) {
-    return { error: 'Expired or invalid token' };
-  }
-};
-
-module.exports = { createToken, validateToken };
+module.exports = { createToken };
