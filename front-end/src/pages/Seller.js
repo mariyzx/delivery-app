4b3529bf1c1ff3786ca18c0b5a8b14e9cc5b74e6
Marco Túlio Huara <marcotuliohuara@gmail.com>
2023-02-02 19:09:29 -0300

    feat: adiciona a pagina de Seller.

---
+++ front-end/src/pages/Seller.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from '../components/Navbar';
+import SellerCard from '../components/SellerCard';
+
+function Seller() {
+  return (
+    <div>
+      <Navbar />
+      <SellerCard />
+    </div>
+  );
+}
+
+export default Seller;
